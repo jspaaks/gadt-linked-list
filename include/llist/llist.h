41a54c281a821e185026ca34d72544281966bf4e
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2024-08-09 10:00:36 -0400

    ran the formatter on include/

--- include/llist/llist.h
+++ include/llist/llist.h
@@ -1,22 +1,23 @@
 #ifndef LLIST_H
 #define LLIST_H
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct llist LinkedList;
+
 typedef struct printers {
     void (*pre)(FILE *, size_t);
     void (*elem)(FILE *, size_t, size_t, void *);
     void (*post)(FILE *, size_t);
 } llist__Printers;
 
-void llist__append(LinkedList *, void *);
+void llist__append (LinkedList *, void *);
 LinkedList * llist__create (void);
-void llist__delete(LinkedList *, bool (*)(void *), bool);
+void llist__delete (LinkedList *, bool (*)(void *), bool);
 void llist__destroy (LinkedList **);
 void llist__insert (LinkedList *, size_t, void *);
 size_t llist__get_length (LinkedList *);
-void llist__prepend(LinkedList *, void *);
-void llist__print(FILE *, LinkedList *, llist__Printers *);
+void llist__prepend (LinkedList *, void *);
+void llist__print (FILE *, LinkedList *, llist__Printers *);
 
 #endif
